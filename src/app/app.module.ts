03806149a1fbdc16615d9eaae0eb30c4a26dac85
Shwshank <shwshank3@gmail.com>
2018-08-04 15:46:49 +0530

    UP Map initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { HttpModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+import { HomeComponent } from './home/home.component';
+import { MapTestComponent } from './map-test/map-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapTestComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,
+    NgxEchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
